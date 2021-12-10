5ff49238c92418e4fc525563cedc2359aabe4088
Alvaro <almunav@gmail.com>
2021-12-10 19:00:41 +0100

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toRemove) {
+    toRemove.forEach(element => {
+        let index = array.findIndex(item => item === element);
+        if (index >= 0) {
+            array.splice(index, 1);
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
