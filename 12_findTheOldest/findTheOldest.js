8b6819cb7bc0706387b821e80fbae3615119095b
Alvaro <almunav@gmail.com>
2021-12-10 23:38:39 +0100

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    let oldest;
+    people.forEach(person => {
+        if (oldest == undefined) {
+            oldest = person;
+        } else {
+            let currentAge = ("yearOfDeath" in person ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth);
+            let oldestAge = ("yearOfDeath" in oldest ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth);
+            if (currentAge > oldestAge) {
+                oldest = person;
+            }
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
