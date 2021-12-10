bacb1289c428ad0923ee69bbc775b4fb54d36182
Alvaro <almunav@gmail.com>
2021-12-10 18:43:52 +0100

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function(str) {
+const reverseString = function (str) {
+    let reversedStr = "";
+    let splitStr = str.split("");
 
+    for (let index = splitStr.length - 1; index >= 0; index--) {
+        reversedStr = reversedStr + splitStr[index];
+    }
+    return reversedStr;
 };
 
 // Do not edit below this line
