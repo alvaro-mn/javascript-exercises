a1d2c7ee10fa39b1c4b0b3164aa101003e7ad4c6
Alvaro <almunav@gmail.com>
2021-12-10 22:37:12 +0100

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let normalized = string.replace(/[!,. ]/g, "").toLowerCase();
+    let chars = normalized.split("");
+    let backwards = "";
+    for (let index = chars.length - 1; index >= 0; index--) {
+        backwards = backwards + chars[index];
+    }
 
+    return normalized === backwards;
 };
 
 // Do not edit below this line
