09830f7605e025be345dd6ea8a71ec53e70920ee
Alvaro <almunav@gmail.com>
2021-12-10 18:37:30 +0100

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    if (num < 0) {
+        return "ERROR";
+    }
 
+    let repeatedString = "";
+    for (let index = 0; index < num; index++) {
+        repeatedString = repeatedString + string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
