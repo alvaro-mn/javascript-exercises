eb2d786721383a89c4aece4ad576e040588ef38b
Alvaro <almunav@gmail.com>
2021-12-10 22:59:08 +0100

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+    sequence = [1, 1, 2];
 
+    if (member < 1) {
+        return "OOPS";
+    }
+    if (member < 4) {
+        return sequence[member - 1];
+    }
+    for (let index = 2; index < member; index++) {
+        sequence.push(sequence[index] + sequence[index - 1]);
+    }
+    return sequence[member - 1];
 };
 
 // Do not edit below this line
