b81d992d61cf263d290d640526726ddb4bbf8f2c
Alvaro <almunav@gmail.com>
2021-12-10 19:11:13 +0100

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (numberFrom, numberTo) {
+    if (numberFrom < 0 || numberTo < 0) {
+        return "ERROR";
+    }
 
+    if (typeof numberFrom != "number" || typeof numberTo != "number") {
+        return "ERROR";
+    }
+
+    if (numberFrom > numberTo) {
+        [numberFrom, numberTo] = [numberTo, numberFrom];
+    }
+
+    sum = 0;
+    for (let index = numberFrom; index <= numberTo; index++) {
+        sum = sum + index;
+    }
+    return sum;
 };
 
 // Do not edit below this line
